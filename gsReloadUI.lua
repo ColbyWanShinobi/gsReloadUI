35edf339d80e0e1b1be0a501356e248f13205c4e
Colby Butler <colby@owljet.com>
2019-09-08 14:20:03 -0400

    Updated for 8.2 and Classic 1.13

--- gsReloadUI.lua
+++ gsReloadUI.lua
@@ -1,13 +1,18 @@
----------------------------
--- GameShaman - ReloadUI --
----------------------------
+--[[
+gsReloadUI
+Created by ColbyWanShinobi
+email: colbywanshinobi@gameshaman.com
+web: gameshaman.com
+repo: https://github.com/ColbyWanShinobi/gsReloadUI.git
+--]]
 
 --Add the slash commands
-SLASH_RELOADUI1 = '/reloadui'
-SLASH_RELOADUI2 = '/rl'
+SLASH_RELOADUI1 = '/reloadui';
+SLASH_RELOADUI2 = '/rl';
 
 SlashCmdList['RELOADUI'] = function(self, txt)
 	ReloadUI();
 end
 
-print("Addon: ReloadUI - (GameShaman) Loaded...")
+print("gsReloadUI by gameshaman.com");
+print("gsReloadUI: Addon Loaded");
